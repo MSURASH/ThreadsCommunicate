f09e2023cf88d005675ddb1d216aef5d04683726
Sufian Rashidi <Sufian100100@gmail.com>
2019-08-11 03:26:09 -0400

    First Load

---
+++ src/ThreadsTalk.java
@@ -0,0 +1,57 @@
+
+public class ThreadsTalk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object lock = new Object();
+	    Thread A = new Thread(new Runnable() {
+	        @Override
+	        public void run() {
+	            synchronized (lock) {
+	                System.out.println("A 1");
+	                try {
+	                    lock.wait();
+	                } catch (InterruptedException e) {
+	                    e.printStackTrace();
+	                }
+	                System.out.println("A 2");
+	                System.out.println("A 3");
+	            }
+	        }
+	    });
+	    Thread B = new Thread(new Runnable() {
+	        @Override
+	        public void run() {
+	            synchronized (lock) {
+	                System.out.println("B 1");
+	                System.out.println("B 2");
+	                System.out.println("B 3");
+                    try {
+						lock.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	            }
+                System.out.println("B 4");
+
+	        }
+	    });
+	    Thread C = new Thread(new Runnable() {
+	        @Override
+	        public void run() {
+	            synchronized (lock) {
+	                System.out.println("C 1");
+	                System.out.println("C 2");
+	                System.out.println("C 3");
+	                lock.notify();
+	            }
+	        }
+	    });
+	    A.start();
+	    B.start();
+	    C.start();
+
+	}
+
+}
